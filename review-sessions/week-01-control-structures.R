c086aeb6de545267e461c5b11f709a8c49bedd0d
Jesse Lecy <jdlecy@gmail.com>
2021-08-24 14:32:40 -0700

    Update week-01-control-structures.R

--- review-sessions/week-01-control-structures.R
+++ review-sessions/week-01-control-structures.R
@@ -22,7 +22,215 @@
 
 
 
-### FUNCTIONS 
+### BASIC PRINCIPLES OF CODE DEVELOPMENT - 
+### TESTING CODES AND ISOLATING BUGS 
+
+# Create a function to calculate exponentials:
+# x is the base number 
+# y is the exponent 
+
+exponentiate <- function( x, y )
+{
+  x.raised.to.y <- x^y
+  return( x.raised.to.y ) 
+}
+
+# order often matters in functions: 
+# these do not produce the same answer
+
+3 ^ 5
+5 ^ 3
+
+
+# FUNCTION DECOMPOSITION 
+# How can you break open the black box to 
+# test the function code? 
+
+
+# argument assignment
+# exponentiate( x=3, y=5 )
+
+x = 3
+y = 5
+
+# function code
+
+x.raised.to.y  <-  x ^ y
+
+# print result at the end instead of 
+# returning it when testing the code   
+# return( x.raised.to.y )
+
+x.raised.to.y 
+
+
+# DECOMPOSITION:
+# function code as a script or recipe: 
+
+x <- 3
+y <- 5 
+x.raised.to.y  <-  x ^ y
+x.raised.to.y
+
+
+
+
+
+
+
+
+
+# DECOMPOSITION OF STEP 3
+
+# step 1: create a vector of doors as the game
+# step 2: condition on doors not to include the picked choice or "cars" 
+# step 3: return the selection
+
+open_goat_door <- function( game, a.pick )
+{
+
+   doors <- c(1,2,3)
+   available.doors <- doors[ doors != a.pick & game != "cars" ]
+   opened.door <- sample(doors, size=1, replace=F)
+   return( opened.door ) # number between 1 and 3
+   
+}
+
+# DECOMPOSITION:
+
+# concrete values for testing 
+
+current.game <- create_game()
+my.pick <- select_door()
+
+# OR 
+
+current.game <- c("goat","car","goat")
+my.pick <- 3
+
+# assign concrete values to generic function placeholders 
+
+game <- current.game 
+a.pick <- my.pick
+
+# function code 
+
+doors <- c(1,2,3)
+available.doors <- doors[ doors != a.pick & game != "cars" ]  
+opened.door <- sample( available.doors, size=1, replace=F )
+opened.door  # single number between 1 and 3
+
+
+# unnest steps to isolate behavior:
+# 
+# four steps in 1 here: 
+# doors[ doors != a.pick & game != "cars" ] 
+
+criteria.01 <- doors != a.pick
+criteria.02 <- game != "cars"
+joint.criteria <- criteria.01 & criteria.02
+available.doors <- doors[ joint.criteria ]  
+
+
+
+# DECOMPOSED FUNCTION: 
+# each line can now be inspected 
+# to isolate the source of 
+# unexpected behaviors 
+
+current.game <- c("goat","car","goat")
+my.pick <- 3
+
+game <- current.game 
+a.pick <- my.pick
+criteria.01 <- doors != a.pick
+criteria.02 <- game != "cars"
+joint.criteria <- criteria.01 & criteria.02
+available.doors <- doors[ joint.criteria ]  
+opened.door <- sample( available.doors, size=1, replace=F )
+opened.door   
+
+
+
+
+
+
+
+# EXPLICIT VS IMPLICIT ARGUMENTS 
+
+# explicit means call each by name 
+
+exponentiate( x=3, y=5 )  # works fine
+exponentiate( y=5, x=3 )  # works fine 
+
+# implicit means call each by position only
+
+exponentiate( 3, 5 )  # works fine
+exponentiate( 5, 3 )  # fails
+
+# implicit assignment occurs in the order of argument defined in the function: 
+
+x = 5
+y = 3
+
+# explicit argument calls are more robust,
+# especially if functions change over time
+
+
+# EXPLICIT ARGUMENT CALLS
+open_goat_door( game=current.game, a.pick=my.pick )
+
+# we assign objects and values at the argument stage
+
+game=current.game, a.pick=my.pick
+
+# are equivalent to: 
+game <- current.game
+a.pick <- my.pick
+
+
+
+# IMPLICIT ARGUMENT CALL
+open_goat_door( current.game, my.pick )
+
+# R will do the following implicitly:  
+
+game <- current.game 
+a.pick <- my.pick
+
+
+# game and a.pick are generic placeholders
+# for whatever values are used for the current game 
+
+
+
+###  BEST PRACTICE:                               
+###  AVOID NAMING TEST OBJECTS SAME AS ARGUMENTS  
+
+current.game <- c("goat","car","goat")
+my.pick <- 3
+
+open_goat_door( game=current.game, a.pick=my.pick )
+
+# NEVER DO THIS: 
+
+game <- c("goat","car","goat")
+a.pick <- 3
+open_goat_door( game=game, a.pick=a.pick )
+
+
+
+
+
+
+
+
+
+
+
+
+
+### FUNCTION REVIEW
 
 * arguments 
 * argument defaults 
@@ -148,32 +356,28 @@ d[  ! ( gender == "male" & beard == "yes" )  ,  ]
 
 
 
-### CONTROL STRUCTURES
-
+### LAB 01 SETUP
 
 
 
 
 ## STEP 1 - CREATE A NEW GAME
 
-```{r}
+
 create_game <- function( )
 {
     a.game <- sample( x=c("goat","goat","car"), size=3, replace=F )
     return( a.game )
 } 
-```
 
 
+# Test of Function:
+# try three times to see randomization
 
-**Test of Function:**
-
-```{r}
-# try three times to see randomization 
 create_game()  
 create_game() 
 create_game() 
-```
+
 
 
 
@@ -181,79 +385,69 @@ create_game()
 
 
 ## STEP 2 - CONTESTANT SELECTS A DOOR 
+# The contestant makes their first selection. Write a function to select one door at random.  
 
-The contestant makes their first selection. Write a function to select one door at random.  
 
-```{r}
 select_door <- function( )
 {
   doors <- c(1,2,3) 
   a.pick <- # YOUR CODE HERE...
   return( a.pick )  # number between 1 and 3
-}
-```
 
 
-```{r}
+
 # test the function
 select_door()
 select_door()
 select_door()
-```
+
 
 
 
 
 
 ## STEP 3 - HOST OPENS GOAT DOOR
+# Note to call this function you need information from previous functions. 
+# The host will always open a door with a goat behind it. But it can't be 
+# a door the contestant has already selected. So it must be a door that is 
+# not a car and not a current contestant selection. 
+#
+# Note that if the contestant selects the car on the first guess the host 
+# can open either door, but if the contestant selects a goat the host only 
+# has one option. 
 
 
-Note to call this function you need information from previous functions. 
 
-The host will always open a door with a goat behind it. But it can't be a door the contestant has already selected. So it must be a door that is not a car and not a current contestant selection. 
-
-Note that if the contestant selects the car on the first guess the host can open either door, but if the contestant selects a goat the host only has one option. 
-
-
-```{r}
 open_goat_door <- function( game, a.pick )
 {
    # YOUR CODE HERE...
    return( opened.door ) # number between 1 and 3
 }
-```
 
-
-
-```{r}
 # test it
 this.game <- create_game()
 this.game
 my.initial.pick <- select_door()
 my.initial.pick
 open_goat_door( this.game, my.initial.pick )
-```
+
 
 
 
 
 ## STEP 4 - CHANGE DOORS
+# The contestant is given the option to change from their initial selection 
+# to the other door that is still closed. The function will represent the 
+# game-playing strategy as the argument **stay=TRUE** or **stay=FALSE** 
 
 
-The contestant is given the option to change from their initial selection to the other door that is still closed. The function will represent the game-playing strategy as the argument **stay=TRUE** or **stay=FALSE**. 
-
-
-```{r}
 change_door <- function( stay=T, opened.door, a.pick )
 {
    # YOUR CODE HERE...
    return( final.pick )  # number between 1 and 3
 }
-```
 
 
-
-```{r}
 # test it
 opened.door <- open_goat_door( this.game, my.initial.pick )
 change_door( stay=T, opened.door=opened.door, a.pick=my.initial.pick )
@@ -262,7 +456,7 @@ my.final.pick <- change_door( stay=F, opened.door=opened.door, a.pick=my.initial
 this.game
 my.initial.pick
 my.final.pick 
-```
+
 
 
 
@@ -270,7 +464,6 @@ my.final.pick
 ## STEP 5 - DETERMINE IF CONTENSTANT HAS WON
 
 
-```{r, eval=F}
 determine_winner <- function( final.pick, game )
 {
    if( ...YOUR CODE HERE... )
@@ -282,10 +475,8 @@ determine_winner <- function( final.pick, game )
       return( "LOSE" )
    }
 }
-```
 
 
-```{r}
 # test code
 this.game
 my.initial.pick
@@ -293,19 +484,16 @@ my.final.pick <- changeDoor( stay=T, opened.door=opened.door, a.pick=my.initial.
 determine_winner( final.pick=my.final.pick, game=this.game )
 my.final.pick <- changeDoor( stay=F, opened.door=opened.door, a.pick=my.initial.pick )
 determine_winner( final.pick=my.final.pick, game=this.game )
-```
 
 
 
 
 
 
-
-<br>
 
 ## TEST THE GAME
 
-```{r}
+
 # your game "recipe" 
 this.game <- create_game()
 my.initial.pick <- select_door()
@@ -330,6 +518,6 @@ paste0( "The opened goat door: ", opened.goat.door )
 paste0( "My final selection: ", my.final.pick.switch )
 paste0( "GAME OUTCOME:" )
 determine_winner( final.pick=my.final.pick.switch, game=this.game )
-```
+
 
 
